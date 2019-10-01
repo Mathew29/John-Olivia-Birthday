344626ae2710905fd55e2267a44ac30c3c922ad6
Mathew Akre <mwakre29@gmail.com>
2019-10-01 13:39:45 -0700

    add data

---
+++ src/data/data.js
@@ -0,0 +1,66 @@
+import picture1 from '../assets/images/FB_IMG_1569873449452.jpg'
+import picture2 from '../assets/images/FB_IMG_1569873463051.jpg'
+import picture3 from '../assets/images/FB_IMG_1569873476231.jpg'
+import picture4 from '../assets/images/FB_IMG_1569873483420.jpg'
+import picture5 from '../assets/images/FB_IMG_1569873508054.jpg'
+import picture6 from '../assets/images/FB_IMG_1569873547177.jpg'
+import picture7 from '../assets/images/FB_IMG_1569873608607.jpg'
+import picture8 from '../assets/images/FB_IMG_1569873618872.jpg'
+import picture9 from '../assets/images/FB_IMG_1569873625565.jpg'
+import picture10 from '../assets/images/IMG_0960.JPG'
+const data = {
+  "pictures": [
+    {
+      "_id": "FB_IMG_1569873449452",
+      "index": 0,
+      "picture": picture1
+    },
+    {
+      "_id": "FB_IMG_1569873463051",
+      "index": 1,
+      "picture": picture2
+    },
+    {
+      "_id": "FB_IMG_1569873476231",
+      "index": 2,
+      "picture": picture3
+    },
+    {
+      "_id": "FB_IMG_1569873483420",
+      "index": 3,
+      "picture": picture4
+    },
+    {
+      "_id": "FB_IMG_1569873508054",
+      "index": 4,
+      "picture": picture5
+    },
+    {
+      "_id": "FB_IMG_1569873547177",
+      "index": 5,
+      "picture": picture6
+    },
+    {
+      "_id": "FB_IMG_1569873608607",
+      "index": 6,
+      "picture": picture7
+    },
+    {
+      "_id": "FB_IMG_1569873618872",
+      "index": 7,
+      "picture": picture8
+    },
+    {
+      "_id": "FB_IMG_1569873625565",
+      "index": 8,
+      "picture": picture9
+    },
+    {
+      "_id": "IMG_0960",
+      "index": 9,
+      "picture": picture10
+    },
+  ]
+}
+
+export default data;
